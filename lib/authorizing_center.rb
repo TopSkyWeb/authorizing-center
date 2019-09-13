22f63d6b606042e53251f0632e0c58a4d6865d1f
Ian Wu <ian.w@topskyweb.com>
2019-09-13 23:00:01 +0800

    init gem

---
+++ lib/authorizing_center.rb
@@ -0,0 +1,6 @@
+require "authorizing_center/version"
+
+module AuthorizingCenter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/authorizing_center/version.rb
@@ -0,0 +1,3 @@
+module AuthorizingCenter
+  VERSION = "0.1.0"
+end
