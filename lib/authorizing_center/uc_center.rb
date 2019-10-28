0725dc5cb90bc8bbd6c94b5c35365a1aa1c51e79
Ian Wu <ian.w@topskyweb.com>
2019-10-28 12:00:47 +0800

    fix base64 error between ruby and php

--- lib/authorizing_center/uc_center.rb
+++ lib/authorizing_center/uc_center.rb
@@ -4,7 +4,7 @@ module AuthorizingCenter
   module UcCenter
     def self.authorize?(username, password)
       site = RestClient::Resource.new(AuthorizingCenter.uc_center_endpoint)
-      string_wait_for_encrypt = {password: password, destime: Time.now.to_i}.to_query
+      string_wait_for_encrypt = {password: password, destime: Time.now.to_i, ip: request.remote_ip}.to_query
       post_filed = {username: username, desurl: AuthorizingCenter::UcCenter.encrypt(string_wait_for_encrypt), m: 'api', a: 'getticket'}
       respond = site.post post_filed.to_query
       if UC_GET_TICKET_ERROR_CODE.has_key?(respond.body)
@@ -23,11 +23,15 @@ module AuthorizingCenter
     end
 
     def self.encrypt(str)
-      iv = OpenSSL::Random.random_bytes(16)
-      key = AuthorizingCenter.uc_center_encrypt_key.ljust(32, 0.chr)
-      cipher = SymmetricEncryption::Cipher.new(key: key, iv: iv, encoding: :base64, always_add_header: false, cipher_name: 'aes-256-cbc')
-      data = cipher.encrypt(str)
-      Base64.urlsafe_encode64("#{data}::#{iv}")
+      # php base64 complies with RFC 1421  , rails base64 complies with RFC 2045, if include + , regenerate again
+      loop do
+        iv = OpenSSL::Random.random_bytes(16)
+        key = AuthorizingCenter.uc_center_encrypt_key.ljust(32, 0.chr)
+        cipher = SymmetricEncryption::Cipher.new(key: key, iv: iv, encoding: :base64, always_add_header: false, cipher_name: 'aes-256-cbc')
+        data = cipher.encrypt(str)
+        encrypt_string = Base64.encode64("#{data}::#{iv}")
+        return encrypt_string unless encrypt_string.include?('+')
+      end
     end
   end
 end
